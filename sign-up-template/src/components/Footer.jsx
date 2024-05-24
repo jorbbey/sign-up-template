f7c4b498e40a3e302928ba93cd8c28ea15520374
jorbbey <jorbbey@gmail.com>
2024-05-24 01:22:14 +0100

    made design changes

--- sign-up-template/src/components/Footer.jsx
+++ sign-up-template/src/components/Footer.jsx
@@ -1,11 +1,11 @@
 import { BsInfoLg } from "react-icons/bs";
 const Footer = () => {
   return (
-    <footer className="w-full flex justify-between items-center md:mt-32 lg:mt-44">
+    <footer className="w-full flex justify-between items-center mt-10">
       <p className="text-gray-400 text-[10px] lg:text-xs m-3">
         Copyright 2021-2022 55tarcompany. All Rights Reserved
       </p>
-      <div className="flex justify-between items-center text-light-blue m-10 w-20">
+      <div className="flex justify-between items-center text-light-blue md:m-5 lg:m-9 w-20">
         <BsInfoLg className="text-xs lg:text-lg" />
         <p className="text-[10px] md:text-xs">Need help?</p>
       </div>
--- sign-up-template/src/components/SideBar.jsx
+++ sign-up-template/src/components/SideBar.jsx
@@ -3,13 +3,13 @@ import handShake from "../assets/Light-brown-in-Jacket10.png";
 const SideBar = () => {
   return (
     <div className="gradient w-full md:w-2/4 lg:w-1/4 flex flex-col justify-center items-center">
-      <img src={logo} alt="logo_image" className="mt-28 md:mt-36 w-1/4 md:w-1/2" />
-      <img src={handShake} alt="handshake" className="-mt-28 md:-mt-20" />
+      <img src={logo} alt="logo_image" className="mt-20 md:mt-10 w-1/4 md:w-1/2" />
+      <img src={handShake} alt="handshake" className="-mt-32 md:-mt-28" />
       <div className="flex flex-col justify-center items-center text-center -mt-20 mb-20">
-        <h1 className="text-xl md:text-3xl text-gray-200 font-semibold w-3/4">
+        <h1 className="text-2xl md:text-3xl text-gray-200 font-semibold w-3/4">
           Partnership for Business Growth
         </h1>
-        <p className="text-gray-400 text-[10px] md:text-xs font-semibold leading-loose my-2 w-3/4 md:w-full ">
+        <p className="text-gray-400 text-[12px] md:text-xs font-semibold leading-loose my-2 w-3/4 md:w-full ">
           Partnerships are pivotal for business growth, enabling companies to
           leverage mutual strengths, share risks, and capitalize on
           opportunities.
